9805c848747ff0c206588afd9836a74a5bef673e
Ridoy Roy <ridoyroycse53engg@gmail.com>
2023-05-04 10:13:46 +0600

    Connect mongodb with the server and get data from the api

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-
+require('dotenv').config();
 const app = express();
 
 // middleWare 
 app.use(cors());
 app.use(express.json());
+
+
+// Connect mongodb  
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.piip8.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// Get data from mongoDb API 
+async function run() {
+    try {
+        const appointmentCollection = client.db('doctors-appointment').collection('time-slots');
+        app.get('/time-slots', async (req, res) => {
+            const query = {};
+            const options = await appointmentCollection.find(query).toArray();
+            res.send(options);
+        })
+    } finally {
+
+    }
+}
+run().catch(console.log);
+
+
+
+
 
 // crteate a get api
 app.get('/', async (req, res) => {
